0cdf30e9c7a4cbb6e9caa5290718cbeb7324f1c5
Austin Larsen <larsenaustinmk4@gmail.com>
2019-03-16 02:04:55 -0400

    adding helpful functions

--- bin/map.js
+++ bin/map.js
@@ -20,24 +20,43 @@ window.onresize = () =>{
 
 (gameLoop=(frame)=>{
     inc--;
+    let G_BDiv = 10;
     let scaledWidth = (width-(width-height));
-    let groundBlockW = scaledWidth/10;
-    let groundBlockH = height/10;
-    let groundBlockY = height-groundBlockH;
-    let floor = groundBlockY - groundBlockH;
+    let blockW = scaledWidth/G_BDiv;
+    let blockH = height/G_BDiv;
+    let groundBlockY = height-blockH;
 
-    let pipeW = groundBlockW*1;
-    let pipeY = groundBlockH*8;
-    let pipeH = groundBlockY-pipeY;
 
     requestAnimationFrame(gameLoop);
     scrnC.fillStyle = "red"
     scrnC.fillRect(0,0,scrn.width,height);
-    for(var i=0;i<scrn.width/groundBlockW;i++){
-        scrnC.drawImage(groundBlock,inc+(i*groundBlockW),groundBlockY,groundBlockW,groundBlockH)
-        if(i===4){
-            scrnC.drawImage(pipe,inc+(i*groundBlockW),pipeY,pipeW,pipeH)
+
+    for(var i=0;i<scrn.width/blockW;i++){
+        function gridClac(x,y,w,h){
+            let grid = {
+                X : x,
+                Y : blockH*((y-G_BDiv)*-1),
+                W : blockW*w,
+                H : height-(blockH*((h-G_BDiv)*-1))
+            };
+            return grid;
+        };
+
+        function drawObj(obj,x,y,w,h){
+
+            let Pos = gridClac(x,y,w,h);
+
+            if(i === Pos.X){
+                scrnC.drawImage(obj,inc+(i*blockW),Pos.Y,Pos.W,Pos.H)
+            }
+
         }
+
+        console.log(pipePos);
+
+        scrnC.drawImage(groundBlock,inc+(i*blockW),groundBlockY,blockW,blockH)
+
+        drawObj(pipe,10,8,1,1)
     }
 })()
 
